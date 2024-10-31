b037b28f66ac269e7387fd61e0112cc4f89a9868
dungbt <dungbt@senprints.com>
2024-10-31 09:40:36 +0700

    update data

--- hls-proxy/bin/hlsd.js
+++ hls-proxy/bin/hlsd.js
@@ -69,7 +69,7 @@ if (middleware.request) {
     } else if (pathname == '/cdn/ping') {
       res.writeHead(200, { 'Content-Type': 'text/plain' });
       const index = getStartCommandParams('lbindex') || 1
-      res.end('OK ' + index);
+      res.end('Proxy running OK' + index);
     } else {
       // Xử lý các request khác ở đây
       res.writeHead(404);
